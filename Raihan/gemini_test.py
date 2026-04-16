78879af2fe2064e13ae419e07b29118da42cb3d3
Raihannaeem <raihannaeem@gmail.com>
2026-04-16 23:59:03 +0530

    hid secrets

--- Raihan/gemini_test.py
+++ Raihan/gemini_test.py
@@ -1,6 +1,5 @@
 from google import genai
 import os
-os.environ["GEMINI_API_KEY"] = "AIzaSyDeOlsnRi7_eR8xM5YpIM-RQ6P6c4bu0tA"
 
 client = genai.Client()
 response = client.models.generate_content(model="gemini-1.5-pro",   contents="What is the capital of France?")
